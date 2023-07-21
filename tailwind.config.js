11ab33aa8f1defa6516d265643c2032a66e24e14
dunn <corepper03@gmail.com>
2023-07-21 18:19:40 +0700

    modified: lightGray hex code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
+  content: ['./src/**/*.{html,js,ts,tsx,jsx}'],
   theme: {
     extend: {
       colors: {
-        primary: "#A18A68",
-        darkGray: "#707070",
-        gray: "#D8D8D8",
-        lightGray: "##EFEFEF",
-        error: "#D82700"
+        primary: '#A18A68',
+        darkGray: '#707070',
+        gray: '#D8D8D8',
+        lightGray: '#EFEFEF',
+        error: '#D82700',
       },
-    fontFamily: {
-      allerta: ['Allerta Stencil', 'sans-serif'], 
-      dmSans: ['DM Sans', 'sans-serif'],
-    }
+      fontFamily: {
+        allerta: ['Allerta Stencil', 'sans-serif'],
+        dmSans: ['DM Sans', 'sans-serif'],
+      },
     },
   },
   plugins: [],
-}
+};
