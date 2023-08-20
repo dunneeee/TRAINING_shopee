af4926d6d03cefdcc8bf6d5dda0d005b97d41168
dunn <corepper03@gmail.com>
2023-08-20 15:17:27 +0700

    add: css to desktop view

--- src/components/Layouts/Footer/Footer.tsx
+++ src/components/Layouts/Footer/Footer.tsx
@@ -8,9 +8,24 @@ interface Props {
 
 export const Footer = ({ className }: Props) => {
   return (
-    <footer className={clsx('', className)}>
-      <div className="footer-top mb-10">
-        <div className="input font-body-medium flex items-center border-b border-black">
+    <footer
+      className={clsx(
+        'flex flex-wrap md:border-t md:border-gray md:py-6',
+        className
+      )}
+    >
+      <div
+        className={clsx(
+          'footer-top mb-10 w-full',
+          'h-fit md:order-2 md:flex md:w-1/2 md:flex-wrap md:justify-end'
+        )}
+      >
+        <div
+          className={clsx(
+            'input font-body-medium flex items-center border-b border-black',
+            'md:w-8/12'
+          )}
+        >
           <input
             className="font-body-medium w-full py-1"
             placeholder="Give an email, get the newsletter."
@@ -23,30 +38,70 @@ export const Footer = ({ className }: Props) => {
           <input type="checkbox" className="mr-1" />
           <span>I agree to the website’s terms and conditions</span>
         </div>
+        <ul className="mt-10 hidden w-full justify-end gap-x-4 md:flex">
+          <li>
+            <Icons.Facebook className="h-5 w-5 " />
+          </li>
+          <li>
+            <Icons.Instagram className="h-5 w-5" />
+          </li>
+          <li>
+            <Icons.Twitter className="h-5 w-5" />
+          </li>
+        </ul>
       </div>
-      <ul className="links mb-10">
-        <Link className="font-body-small mb-2 block" to="/contact">
+      <ul
+        className={clsx(
+          'links mb-10 flex w-full flex-wrap',
+          'md:order-1 md:w-1/2 md:gap-x-10'
+        )}
+      >
+        <Link
+          className={clsx(
+            'font-body-small mb-2 block w-full',
+            'md:font-body-large md:w-auto md:font-normal md:text-darkGray'
+          )}
+          to="/contact"
+        >
           CONTACT
         </Link>
-        <Link className="font-body-small mb-2 block">TERMS OF SERVICES</Link>
-        <Link className="font-body-small mb-2 block">SHIPPING AND RETURNS</Link>
+        <Link
+          className={clsx(
+            'font-body-small mb-2 block w-full',
+            'md:font-body-large md:w-auto md:font-normal md:text-darkGray'
+          )}
+        >
+          TERMS OF SERVICES
+        </Link>
+        <Link
+          className={clsx(
+            'font-body-small mb-2 block w-full',
+            'md:font-body-large md:w-auto md:font-normal md:text-darkGray'
+          )}
+        >
+          SHIPPING AND RETURNS
+        </Link>
+        <li className="mt-10 w-full">
+          <div className="follows_us mb-10 flex items-center md:hidden">
+            <span>Follow us</span>
+            <span className="line ml-3 inline-block w-12"></span>
+            <div className="flex">
+              <Link className="ml-2">
+                <Icons.Facebook />
+              </Link>
+              <Link className="ml-4">
+                <Icons.Instagram />
+              </Link>
+              <Link className="ml-4">
+                <Icons.Twitter />
+              </Link>
+            </div>
+          </div>
+          <p className="mb-4">
+            © 2020 Shelly. Terms of use and privacy policy.
+          </p>
+        </li>
       </ul>
-      <div className="follows_us mb-10 flex items-center">
-        <span>Follow us</span>
-        <span className="line ml-3 inline-block w-12"></span>
-        <div className="flex">
-          <Link className="ml-2">
-            <Icons.Facebook />
-          </Link>
-          <Link className="ml-4">
-            <Icons.Instagram />
-          </Link>
-          <Link className="ml-4">
-            <Icons.Twitter />
-          </Link>
-        </div>
-      </div>
-      <p className="mb-4">© 2020 Shelly. Terms of use and privacy policy.</p>
     </footer>
   );
 };
