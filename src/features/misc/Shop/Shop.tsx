e9fe4d350c1eb5a3a66467e9acb04d00858e3c2b
dunn <corepper03@gmail.com>
2023-08-21 09:04:29 +0700

    add: desktop view for shop page

--- src/features/misc/Shop/Shop.tsx
+++ src/features/misc/Shop/Shop.tsx
@@ -1,15 +1,43 @@
 import { ProductData, ProductGrid } from '@/components/Product';
 import { FilterButton } from './components';
 import { useProduct, useScrollTop } from '@/hooks';
+import { InputField, InputSelect } from '@/components/Form';
 
 const Shop = () => {
   useScrollTop();
   const { productState } = useProduct();
   return (
-    <section className="wrapper">
+    <section className="wrapper overflow-hidden">
       <h3 className="mb-4">Shop</h3>
-      <FilterButton className="mb-4" />
-      <ProductGrid products={productState.sortProducts as ProductData[]} />
+      <div className="-m-2 flex flex-wrap">
+        <div className="w-full p-2 md:w-3/12">
+          <InputField placeholder="Search" className="hidden md:block" />
+          <FilterButton className="mb-2 block md:hidden" />
+          <div className="filter-box">
+            <InputSelect
+              className="mt-8 hidden w-full md:block"
+              shape="square"
+              options={[
+                {
+                  label: 'Sort By',
+                  value: '',
+                },
+                {
+                  label: 'Name',
+                  value: 'name',
+                },
+                {
+                  label: 'Price',
+                  value: 'price',
+                },
+              ]}
+            />
+          </div>
+        </div>
+        <div className="w-full p-2 md:w-9/12">
+          <ProductGrid products={productState.sortProducts as ProductData[]} />
+        </div>
+      </div>
     </section>
   );
 };
